58417bb3ee0fc24794f3db3651c6dcb6fb78c9f8
Wolfgang Bock <wolfgang.bock@htwchur.ch>
2020-02-10 11:14:27 +0100

    Start Project

---
+++ AJAX/bus_chur/buslinien.php
@@ -0,0 +1,334 @@
+<?php
+$linie1a = '{
+      "id" : "linie1a",
+      "liniennummer" : 1,
+      "kurzbeschreibung" : "Lachen – Bahnhofplatz – Plankis – Domat/Ems – Rhäzüns",
+      "haltestellen" : [
+        "Turnerwiese",
+        "Amselweg",
+        "Wiesental",
+        "Giacomettistrasse",
+        "Lachen",
+        "Lacuna",
+        "Tulpenweg",
+        "Daleustrasse",
+        "Post 1",
+        "Bahnhofplatz",
+        "Postplatz",
+        "Malteser",
+        "Brambrüeschbahn/Stadthalle",
+        "Kaserne/Kornquader",
+        "Strassenverkehrsamt",
+        "Kalchbühl",
+        "Sommerau",
+        "Plankis",
+        "Felsberg, Emserstrasse",
+        "Domat/Ems, Abzw. Felsberg",
+        "Domat/Ems, Plarenga",
+        "Domat/Ems, Unterdorf",
+        "Domat/Ems, Plaz",
+        "Domat/Ems, Post",
+        "Domat/Ems, Tuma Platta",
+        "Domat/Ems, Padrusa",
+        "Domat/Ems, Ems Chemie",
+        "Tamins, Unterdorf",
+        "Reichenau, Adler",
+        "Bonaduz, Isla",
+        "Bonaduz, Alte Post",
+        "Bonaduz, Crusch",
+        "Rhäzüns, Dorfplatz",
+        "Rhäzüns, Luftseilbahn"
+      ]
+    }';
+$linie1b = '{
+      "id" : "linie1b",
+      "liniennummer" : 1,
+      "kurzbeschreibung" : "Lachen – Bahnhofplatz – Plankis – Felsberg",
+      "haltestellen" : [
+        "Turnerwiese",
+        "Amselweg",
+        "Wiesental",
+        "Giacomettistrasse",
+        "Lachen",
+        "Lacuna",
+        "Tulpenweg",
+        "Daleustrasse",
+        "Post 1",
+        "Bahnhofplatz",
+        "Postplatz",
+        "Malteser",
+        "Brambrüeschbahn/Stadthalle",
+        "Kaserne/Kornquader",
+        "Strassenverkehrsamt",
+        "Kalchbühl",
+        "Sommerau",
+        "Plankis",
+        "Felsberg, Emserstrasse",
+        "Felsberg, Abzweigung",
+        "Felsberg, Bahnhof",
+        "Felsberg, Brugg",
+        "Felsberg, Calanda",
+        "Felsberg, Schöneggstrasse",
+        "Felsberg, Bahnhofstrasse",
+        "Felsberg, Post",
+        "Felsberg, Underchrüzli",
+        "Felsberg, Kirchenstutz",
+        "Felsberg, Ringelweg",
+        "Felsberg, Rjterstutz/Aeuli"
+      ]
+    }';
+$linie2a = '{
+      "id" : "linie2a",
+      "liniennummer" : 2,
+      "kurzbeschreibung" : "Obere Au – Bahnhofplatz – Fürstenwald",
+      "haltestellen" : [
+        "Kleinbruggen",
+        "Rheinfels",
+        "Sommeraustr. Rossboden",
+        "Medienhaus/Rossboden",
+        "Rossboden",
+        "Obere Au P&#038;R",
+        "Obere Au",
+        "Grossbruggerweg",
+        "Industriestrasse",
+        "HTW Ringstrasse",
+        "Grischunaweg",
+        "Güterstrasse",
+        "Sägenstrasse",
+        "Rätusstrasse",
+        "Benerpark",
+        "Post 1",
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Schulhaus Quader",
+        "Berggasse",
+        "Meierweg",
+        "Lochertstrasse",
+        "Fontanaspital",
+        "Arlibonstrasse",
+        "Fürstenwaldstrasse",
+        "Fürstenwald"
+      ]
+    }';
+$linie2b = '{
+      "id" : "linie2b",
+      "liniennummer" : 2,
+      "kurzbeschreibung" : "Obere Au – Bahnhofplatz – Kleinwaldegg",
+      "haltestellen" : [
+        "Kleinbruggen",
+        "Rheinfels",
+        "Sommeraustr. Rossboden",
+        "Medienhaus/Rossboden",
+        "Rossboden",
+        "Obere Au P&#038;R",
+        "Obere Au",
+        "Grossbruggerweg",
+        "Industriestrasse",
+        "HTW Ringstrasse",
+        "Grischunaweg",
+        "Güterstrasse",
+        "Sägenstrasse",
+        "Rätusstrasse",
+        "Benerpark",
+        "Post 1",
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Schulhaus Quader",
+        "Berggasse",
+        "Meierweg",
+        "Lochertstrasse",
+        "Bondastrasse",
+        "Eulengut",
+        "Plantaweg",
+        "Heckenweg",
+        "Kleinwaldegg",
+        "Böschenstrasse",
+        "Fondeiweg",
+        "Fontanaspital"
+      ]
+    }';
+$linie3a = '{
+      "id" : "linie3a",
+      "liniennummer" : 3,
+      "kurzbeschreibung" : "Austrasse – Bahnhofplatz – Stelleweg – Haldenstein",
+      "haltestellen" : [
+        "Oberalpstrasse",
+        "Florentini",
+        "Fortunastrasse",
+        "Sardona",
+        "Altes Gaswerk",
+        "Austrasse",
+        "Scawoba",
+        "Albula",
+        "Schönmattweg",
+        "Gewerbliche Berufsschule",
+        "Post 1",
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Quaderwiese",
+        "Rigahaus",
+        "Kreuzgasse",
+        "Weinbergstrasse",
+        "Heiligkreuz",
+        "Weisstorkel",
+        "Masans Kirche",
+        "Stelleweg",
+        "Haldenstein, Rheinbrücke",
+        "Haldenstein, Hanfländer",
+        "Haldenstein, Dorf",
+        "Haldenstein, Schulhaus",
+        "Haldenstein, Usserdorfstrasse"
+      ]
+    }';
+$linie3b = '{
+      "id" : "linie3b",
+      "liniennummer" : 3,
+      "kurzbeschreibung" : "Austrasse – Bahnhofplatz – Trimmis – Untervaz",
+      "haltestellen" : [
+        "Oberalpstrasse",
+        "Florentini",
+        "Fortunastrasse",
+        "Sardona",
+        "Altes Gaswerk",
+        "Austrasse",
+        "Scawoba",
+        "Albula",
+        "Schönmattweg",
+        "Gewerbliche Berufsschule",
+        "Post 1",
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Quaderwiese",
+        "Rigahaus",
+        "Kreuzgasse",
+        "Weinbergstrasse",
+        "Heiligkreuz",
+        "Weisstorkel",
+        "Masans Kirche",
+        "Oldtimer",
+        "Halbmil",
+        "Trimmis, Churweg",
+        "Trimmis, Saliet",
+        "Trimmis, Gargällis",
+        "Trimmis, Alt Strass",
+        "Zizers, Tennishalle",
+        "Untervaz, Bahnhof",
+        "Untervaz, Holcim",
+        "Untervaz, Kirchgasse",
+        "Untervaz, Dorf",
+        "Untervaz, Giesacker",
+        "Untervaz, Gaidla"
+      ]
+    }';
+$linie4 = '{
+      "id" : "linie4",
+      "liniennummer" : 4,
+      "kurzbeschreibung" : "Austrasse – Bahnhofplatz – Seniorenzentrum Cadonau",
+      "haltestellen" : [
+        "Badusstrasse",
+        "Schönmattweg",
+        "Albula",
+        "Scawoba",
+        "Austrasse",
+        "Altes Gaswerk",
+        "Sardona",
+        "Fortunastrasse",
+        "Florentini",
+        "Oberalpstrasse",
+        "Post 1",
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Schulhaus Quader",
+        "Neubruchstrasse",
+        "Splügenstrasse",
+        "Sonnenbergstrasse",
+        "Kreuzspital",
+        "Calunastrasse",
+        "Rosengarten",
+        "Kantonsspital",
+        "Waldhaus",
+        "Cadonaustrasse",
+        "Bürgerheim",
+        "Seniorenzentrum Cadonau"
+      ]
+    }';
+$linie6 = '{
+      "id" : "linie6",
+      "liniennummer" : 6,
+      "kurzbeschreibung" : "Bahnhofplatz – City West",
+      "haltestellen" : [
+        "Bahnhofplatz",
+        "Post 1",
+        "Benerpark",
+        "Rätusstrasse",
+        "Boletta",
+        "Salvatorenstrasse",
+        "City West",
+        "Medienhaus",
+        "Sommeraustr. Rossboden",
+        "Rheinfels",
+        "Kleinbruggen"
+      ]
+    }';
+$linie9 = '{
+      "id" : "linie9",
+      "liniennummer" : 9,
+      "kurzbeschreibung" : "Bahnhofplatz – Meiersboden",
+      "haltestellen" : [
+        "Bahnhofplatz",
+        "Alexanderplatz",
+        "Quader Center",
+        "Reichsgasse",
+        "Planaterrastrasse",
+        "Hof",
+        "Turnhalle Sand",
+        "Sandstrasse",
+        "Krematorium",
+        "Plessurfall",
+        "Sassal",
+        "Rabiosabrücke",
+        "Meiersboden",
+        "Bodmer",
+        "Malteser",
+        "Fontanaplatz",
+        "Postplatz"
+      ]
+    }';
+
+$linien_id = $_GET['linienid'];
+switch ($linien_id) {
+    case "linie1a":
+        $rueckgabe = $linie1a;
+        break;
+    case "linie1b":
+        $rueckgabe = $linie1b;
+        break;
+    case "linie2a":
+        $rueckgabe = $linie2a;
+        break;
+    case "linie2b":
+        $rueckgabe = $linie2b;
+        break;
+    case "linie3a":
+        $rueckgabe = $linie3a;
+        break;
+    case "linie3b":
+        $rueckgabe = $linie3b;
+        break;
+    case "linie4":
+        $rueckgabe = $linie4;
+        break;
+    case "linie6":
+        $rueckgabe = $linie6;
+        break;
+    case "linie9":
+        $rueckgabe = $linie9;
+        break;
+}
+
+echo $rueckgabe;
